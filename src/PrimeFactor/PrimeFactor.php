2910473907f9e06066606fa50ce69f9a1ef3d303
bandi-k <andras.kollar@gmail.com>
2014-09-23 21:25:07 +0200

    Prime factor test...again+count divisors test.

---
+++ src/PrimeFactor/PrimeFactor.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Contains the prime related functions.
+ */
+
+namespace Kata\PrimeFactor;
+
+class PrimeFactor
+{
+	/**
+	 * Returns prime factors of a number.
+	 *
+	 * @param int $number   The number.
+	 *
+	 * @return array   The array of prime factors.
+	 */
+	function getPrimeFactor($number) {
+		$divisor = 2;
+		$factors = [];
+		while ($number !== 1)
+		{
+			if ($number % $divisor === 0)
+			{
+				$factors[] = $divisor;
+				$number /= $divisor;
+			}
+			else
+			{
+				$divisor++;
+			}
+		}
+
+		return $factors;
+	}
+
+	/**
+	 * Returns a number's divisors count.
+	 *
+	 * @param int $number   The number.
+	 *
+	 * @return int   The count of divisors.
+	 */
+	public function getCountOfDivisors($number) {
+
+		$primeFactors = $this->getPrimeFactor($number);
+
+		$primeCounts = array_count_values($primeFactors);
+		$divisors    = 1;
+
+		foreach ($primeCounts as $primeCount)
+		{
+			$divisors *= ++$primeCount;
+		}
+
+		return $divisors;
+	}
+}
---
+++ test/PrimeFactror/PrimeFactorTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Kata\Test\PrimeFactor;
+
+use Kata\PrimeFactor\PrimeFactor;
+
+class PrimeFactorTest extends \PHPUnit_Framework_TestCase
+{
+
+	public function testPrimeFactor()
+	{
+		$primeFactor = new PrimeFactor();
+
+		$this->assertEquals(array(2), $primeFactor->getPrimeFactor(2));
+		$this->assertEquals(array(3), $primeFactor->getPrimeFactor(3));
+		$this->assertEquals(array(2,2), $primeFactor->getPrimeFactor(4));
+		$this->assertEquals(array(2,3), $primeFactor->getPrimeFactor(6));
+		$this->assertEquals(array(3,3), $primeFactor->getPrimeFactor(9));
+		$this->assertEquals(array(2,2,3), $primeFactor->getPrimeFactor(12));
+		$this->assertEquals(array(3,5), $primeFactor->getPrimeFactor(15));
+	}
+
+	public function testCountDivisors()
+	{
+		$primeFactor = new PrimeFactor();
+
+		$this->assertEquals(2, $primeFactor->getCountOfDivisors(2));
+		$this->assertEquals(2, $primeFactor->getCountOfDivisors(3));
+		$this->assertEquals(3, $primeFactor->getCountOfDivisors(4));
+		$this->assertEquals(4, $primeFactor->getCountOfDivisors(6));
+		$this->assertEquals(3, $primeFactor->getCountOfDivisors(9));
+		$this->assertEquals(6, $primeFactor->getCountOfDivisors(12));
+		$this->assertEquals(4, $primeFactor->getCountOfDivisors(15));
+	}
+}

b64aafe2525209c9220d572d09997f694d2fbe91
bandi-k <andras.kollar@gmail.com>
2014-09-23 21:59:47 +0200

    Hundred doors test and prime factor test refact.

--- test/HundredDoorsTest.php
+++ test/HundredDoorsTest.php
@@ -11,35 +11,40 @@ use Kata\PrimeFactor\PrimeFactor;
 class HundredDoorsTest extends \PHPUnit_Framework_TestCase
 {
 
+	/** @var HundredDoors   The HundredDoors instance. */
+	protected $hundredDoors = null;
+
+	protected function setUp()
+	{
+		$this->hundredDoors = new HundredDoors();
+	}
+
 	public function testHundredDoors()
 	{
-		$hundredDoors = new HundredDoors();
+		$this->assertEquals(array(true), $this->hundredDoors->getDoors(1));
 
-		$this->assertEquals(array(true), $hundredDoors->getDoors(1));
+		$this->assertEquals(array(true, false), $this->hundredDoors->getDoors(2));
 
-		$this->assertEquals(array(true, false), $hundredDoors->getDoors(2));
+		$this->assertEquals(array(true, false, false), $this->hundredDoors->getDoors(3));
 
-		$this->assertEquals(array(true, false, false), $hundredDoors->getDoors(3));
+		$this->assertEquals(array(true, false, false, true), $this->hundredDoors->getDoors(4));
 
-		$this->assertEquals(array(true, false, false, true), $hundredDoors->getDoors(4));
+		$this->assertEquals(array(true, false, false, true, false), $this->hundredDoors->getDoors(5));
 
-		$this->assertEquals(array(true, false, false, true, false), $hundredDoors->getDoors(5));
+		$this->assertEquals(array(true, false, false, true, false, false), $this->hundredDoors->getDoors(6));
 
-		$this->assertEquals(array(true, false, false, true, false, false), $hundredDoors->getDoors(6));
+		$this->assertEquals(array(true, false, false, true, false, false, false), $this->hundredDoors->getDoors(7));
 
-		$this->assertEquals(array(true, false, false, true, false, false, false), $hundredDoors->getDoors(7));
+		$this->assertEquals(array(true, false, false, true, false, false, false, false), $this->hundredDoors->getDoors(8));
 
-		$this->assertEquals(array(true, false, false, true, false, false, false, false), $hundredDoors->getDoors(8));
-
-		$this->assertEquals(array(true, false, false, true, false, false, false, false, true), $hundredDoors->getDoors(9));
+		$this->assertEquals(array(true, false, false, true, false, false, false, false, true), $this->hundredDoors->getDoors(9));
 	}
 
 	public function testHundredDoorsForSure()
 	{
-		$hundredDoors = new HundredDoors();
 		$primeFactor  = new PrimeFactor();
 
-		$actuallyHundredDoors = $hundredDoors->getDoors(100);
+		$actuallyHundredDoors = $this->hundredDoors->getDoors(100);
 
 		$door100 = $primeFactor->isOddNumber($primeFactor->getCountOfDivisors(100));
 		$this->assertEquals($actuallyHundredDoors[99], $door100);
--- test/PrimeFactror/PrimeFactorTest.php
+++ test/PrimeFactror/PrimeFactorTest.php
@@ -7,39 +7,41 @@ use Kata\PrimeFactor\PrimeFactor;
 class PrimeFactorTest extends \PHPUnit_Framework_TestCase
 {
 
+	/** @var PrimeFactor   The PrimerFactor instance. */
+	protected $primeFactor = null;
+
+	protected function setUp()
+	{
+		$this->primeFactor = new PrimeFactor();
+	}
+
 	public function testPrimeFactor()
 	{
-		$primeFactor = new PrimeFactor();
-
-		$this->assertEquals(array(2), $primeFactor->getPrimeFactor(2));
-		$this->assertEquals(array(3), $primeFactor->getPrimeFactor(3));
-		$this->assertEquals(array(2,2), $primeFactor->getPrimeFactor(4));
-		$this->assertEquals(array(2,3), $primeFactor->getPrimeFactor(6));
-		$this->assertEquals(array(3,3), $primeFactor->getPrimeFactor(9));
-		$this->assertEquals(array(2,2,3), $primeFactor->getPrimeFactor(12));
-		$this->assertEquals(array(3,5), $primeFactor->getPrimeFactor(15));
+		$this->assertEquals(array(2), $this->primeFactor->getPrimeFactor(2));
+		$this->assertEquals(array(3), $this->primeFactor->getPrimeFactor(3));
+		$this->assertEquals(array(2,2), $this->primeFactor->getPrimeFactor(4));
+		$this->assertEquals(array(2,3), $this->primeFactor->getPrimeFactor(6));
+		$this->assertEquals(array(3,3), $this->primeFactor->getPrimeFactor(9));
+		$this->assertEquals(array(2,2,3), $this->primeFactor->getPrimeFactor(12));
+		$this->assertEquals(array(3,5), $this->primeFactor->getPrimeFactor(15));
 	}
 
 	public function testCountDivisors()
 	{
-		$primeFactor = new PrimeFactor();
-
-		$this->assertEquals(2, $primeFactor->getCountOfDivisors(2));
-		$this->assertEquals(2, $primeFactor->getCountOfDivisors(3));
-		$this->assertEquals(3, $primeFactor->getCountOfDivisors(4));
-		$this->assertEquals(4, $primeFactor->getCountOfDivisors(6));
-		$this->assertEquals(3, $primeFactor->getCountOfDivisors(9));
-		$this->assertEquals(6, $primeFactor->getCountOfDivisors(12));
-		$this->assertEquals(4, $primeFactor->getCountOfDivisors(15));
+		$this->assertEquals(2, $this->primeFactor->getCountOfDivisors(2));
+		$this->assertEquals(2, $this->primeFactor->getCountOfDivisors(3));
+		$this->assertEquals(3, $this->primeFactor->getCountOfDivisors(4));
+		$this->assertEquals(4, $this->primeFactor->getCountOfDivisors(6));
+		$this->assertEquals(3, $this->primeFactor->getCountOfDivisors(9));
+		$this->assertEquals(6, $this->primeFactor->getCountOfDivisors(12));
+		$this->assertEquals(4, $this->primeFactor->getCountOfDivisors(15));
 	}
 
 	public function testIsEvenNumber()
 	{
-		$primeFactor = new PrimeFactor();
-
-		$this->assertFalse($primeFactor->isOddNumber(6));
-		$this->assertTrue($primeFactor->isOddNumber(11));
-		$this->assertFalse($primeFactor->isOddNumber(2000006));
-		$this->assertTrue($primeFactor->isOddNumber(789789711));
+		$this->assertFalse($this->primeFactor->isOddNumber(6));
+		$this->assertTrue($this->primeFactor->isOddNumber(11));
+		$this->assertFalse($this->primeFactor->isOddNumber(2000006));
+		$this->assertTrue($this->primeFactor->isOddNumber(789789711));
 	}
 }

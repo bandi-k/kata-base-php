196fd132bd83a45b2d82618e9a1560ec1ff24878
bandi-k <andras.kollar@gmail.com>
2014-10-12 19:22:22 +0200

    Iprange check + ttl.

--- src/VelocityChecker/VelocityChecker.php
+++ src/VelocityChecker/VelocityChecker.php
@@ -9,34 +9,86 @@ namespace Kata\VelocityChecker;
 class VelocityChecker {
 
 	/** The failed login limits. */
-	const FAILED_LOGIN_LIMIT_IP = 3;
+	const FAILED_LOGIN_LIMIT_IP       = 3;
+	const FAILED_LOGIN_LIMIT_IP_RANGE = 5;
 
-	/**
-	 * Contains the failed attempts.
-	 *
-	 * @var array
-	 */
-	protected $failedLogins = array();
+	/** The failed login attempts ttl. */
+	const FAILED_LOGIN_ATTEMPTS_TTL = 3600;
+
+	/** @var array   Contains the failed login attempts ips. */
+	protected $failedLoginsIps      = array();
+	/** @var array   Contains the failed login attempts ip ranges. */
+	protected $failedLoginsIpRanges = array();
 
 	/**
 	 * Checks the captcha is needed by ip.
 	 *
-	 * @param string $ip   The ip address.
+	 * @param string $ip     The ip address.
+	 * @param int    $time   The login timestamp.
 	 *
 	 * @return bool   TRUE, if captcha is needed, otherwise FALSE.
 	 */
-	public function isCaptchaNeededByIp($ip)
+	public function isCaptchaNeededByIp($ip, $time)
 	{
-		if (isset($this->failedLogins[$ip]))
+		$this->failedLoginsIps[] = array(
+			'ip'   => $ip,
+			'time' => $time,
+		);
+
+		$attemptCount        = 0;
+		$attemptValidityTime = time() - self::FAILED_LOGIN_ATTEMPTS_TTL;
+
+		foreach ($this->failedLoginsIps as $attempt)
 		{
-			$this->failedLogins[$ip]++;
+			if ($attempt['ip'] !== $ip)
+			{
+				continue;
+			}
 
-			return $this->failedLogins[$ip] >= self::FAILED_LOGIN_LIMIT_IP;
+			if (
+				$attempt['time'] >= $attemptValidityTime
+				&& ++$attemptCount >= self::FAILED_LOGIN_LIMIT_IP
+			) {
+				return true;
+			}
 		}
-
-		$this->failedLogins[$ip] = 1;
 
 		return false;
 	}
 
+	/**
+	 * Checks the captcha is needed by ip range.
+	 *
+	 * @param string $ipRange   The ip address range.
+	 * @param int    $time      The login timestamp.
+	 *
+	 * @return bool   TRUE, if captcha is needed, otherwise FALSE.
+	 */
+	public function isCaptchaNeededByIpRange($ipRange, $time)
+	{
+		$this->failedLoginsIpRanges[] = array(
+			'ip_range' => $ipRange,
+			'time'     => $time,
+		);
+
+		$attemptCount        = 0;
+		$attemptValidityTime = time() - self::FAILED_LOGIN_ATTEMPTS_TTL;
+
+		foreach ($this->failedLoginsIpRanges as $attempt)
+		{
+			if ($attempt['ip_range'] !== $ipRange)
+			{
+				continue;
+			}
+
+			if (
+				$attempt['time'] >= $attemptValidityTime
+				&& ++$attemptCount >= self::FAILED_LOGIN_LIMIT_IP_RANGE
+			) {
+				return true;
+			}
+		}
+
+		return false;
+	}
 } 
--- test/VelocityChecker/VelocityCheckerTest.php
+++ test/VelocityChecker/VelocityCheckerTest.php
@@ -9,13 +9,28 @@ class VelocityCheckerTest extends \PHPUnit_Framework_TestCase{
 	public function testCheckByIp()
 	{
 		$velocityChecker = new VelocityChecker();
+		$time            = time();
 
-		$this->assertFalse($velocityChecker->isCaptchaNeededByIp('192.168.0.1'));
-		$this->assertFalse($velocityChecker->isCaptchaNeededByIp('192.168.0.1'));
-		$this->assertTrue($velocityChecker->isCaptchaNeededByIp('192.168.0.1'));
-		$this->assertTrue($velocityChecker->isCaptchaNeededByIp('192.168.0.1'));
-		$this->assertFalse($velocityChecker->isCaptchaNeededByIp('192.168.0.2'));
-		$this->assertFalse($velocityChecker->isCaptchaNeededByIp('192.168.0.2'));
-		$this->assertTrue($velocityChecker->isCaptchaNeededByIp('192.168.0.2'));
+		$this->assertFalse($velocityChecker->isCaptchaNeededByIp('192.168.0.1', $time));
+		$this->assertFalse($velocityChecker->isCaptchaNeededByIp('192.168.0.1', $time));
+		$this->assertFalse($velocityChecker->isCaptchaNeededByIp('192.168.0.1', $time - 7000));
+		$this->assertTrue($velocityChecker->isCaptchaNeededByIp('192.168.0.1', $time - 2000));
+
+		$this->assertFalse($velocityChecker->isCaptchaNeededByIp('192.168.0.2', $time));
+		$this->assertFalse($velocityChecker->isCaptchaNeededByIp('192.168.0.2', $time));
+		$this->assertTrue($velocityChecker->isCaptchaNeededByIp('192.168.0.2', $time));
+	}
+
+	public function testCheckByIpRange()
+	{
+		$velocityChecker = new VelocityChecker();
+		$time            = time();
+
+		$this->assertFalse($velocityChecker->isCaptchaNeededByIpRange('192.168.0', $time));
+		$this->assertFalse($velocityChecker->isCaptchaNeededByIpRange('192.168.0', $time));
+		$this->assertFalse($velocityChecker->isCaptchaNeededByIpRange('192.168.0', $time));
+		$this->assertFalse($velocityChecker->isCaptchaNeededByIpRange('192.168.0', $time));
+		$this->assertFalse($velocityChecker->isCaptchaNeededByIpRange('192.168.0', $time - 3601));
+		$this->assertTrue($velocityChecker->isCaptchaNeededByIpRange('192.168.0', $time - 3600));
 	}
 } 

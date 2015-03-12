641585e11941c38547df942116ff1445e6c90e12
Andras Kollar <andras.kollar@doclerholding.com>
2015-03-12 18:00:30 +0100

    2 Create stub....doesnt work

--- test/StringToArray/MultiLineStringToArrayTest.php
+++ test/StringToArray/MultiLineStringToArrayTest.php
@@ -13,6 +13,30 @@ class MultiLineStringToArrayTest extends \PHPUnit_Framework_TestCase
 {
 	/**
 	 * Test for multi line string to array.
+	 */
+	public function testMultiLineStringToArrayWithStub()
+	{
+		$stringToArray = $this->getMockBuilder('Kata\StringToArray\StringToArray')->getMock();
+
+		$stringToArray
+			->expects($this->at(0))
+			->method('convert')
+			->with("211,22,35")
+			->will($this->returnValue(array('211','22','35')));
+		$stringToArray
+			->expects($this->at(1))
+			->method('convert')
+			->with("10,20,33")
+			->will($this->returnValue(array('10,20,33')));
+
+		$multiLineStringToArray = new MultiLineStringToArray($stringToArray);
+		$result                 = $multiLineStringToArray->convert("211,22,35\n10,20,33");
+
+		$this->assertEquals(array(array('211,22,35', '10,20,33'), array('211','22','35','10','20','33')), $result);
+	}
+
+	/**
+	 * Test for multi line string to array.
 	 *
 	 * @param string $values          The values string.
 	 * @param array  $expectedArray   The expected array.
@@ -21,12 +45,6 @@ class MultiLineStringToArrayTest extends \PHPUnit_Framework_TestCase
 	 */
 	public function testMultiLineStringToArray($values, $expectedArray)
 	{
-		/*$stringToArray = $this->getMockBuilder('Kata\StringToArray\StringToArray')->getMock();
-
-		$stringToArray
-			->expects($this->exactly(2))
-			->method('convert');*/
-
 		$multiLineStringToArray = new MultiLineStringToArray(new StringToArray());
 		$result                 = $multiLineStringToArray->convert($values);
 

e941dc947eb27f5ec4c99d765a229c454abadb08
Andras Kollar <andras.kollar@doclerholding.com>
2015-03-12 16:47:39 +0100

    1 - Validator refact.

--- src/StringToArray/StringToArray.php
+++ src/StringToArray/StringToArray.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * String to array class.
  */
@@ -12,18 +13,28 @@ class StringToArray
 	 * @param string $values   The string of values.
 	 *
 	 * @return array   The array of values.
+	 */
+	public function convert($values)
+	{
+		$this->validateInputValues($values);
+
+		$array = explode(',', $values);
+
+		return $array;
+	}
+
+	/**
+	 * Validates the input type.
+	 *
+	 * @param mixed $values   The input values.
 	 *
 	 * @throws InvalidInputTypeException   In case of invalid input type.
 	 */
-	public function convert($values)
+	protected function validateInputValues($values)
 	{
 		if (!is_string($values))
 		{
 			throw New InvalidInputTypeException('Wrong input type');
 		}
-
-		$array = explode(',', $values);
-
-		return $array;
 	}
 }

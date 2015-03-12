f9156159ec9b1f1efec4e38d6bcf9f852c9b39e1
Andras Kollar <andras.kollar@doclerholding.com>
2015-03-12 17:45:08 +0100

    2 Data provider.

--- test/StringToArray/MultiLineStringToArrayTest.php
+++ test/StringToArray/MultiLineStringToArrayTest.php
@@ -13,8 +13,13 @@ class MultiLineStringToArrayTest extends \PHPUnit_Framework_TestCase
 {
 	/**
 	 * Test for multi line string to array.
+	 *
+	 * @param string $values          The values string.
+	 * @param array  $expectedArray   The expected array.
+	 *
+	 * @dataProvider inputValuesProvider
 	 */
-	public function testMultiLineStringToArray()
+	public function testMultiLineStringToArray($values, $expectedArray)
 	{
 		/*$stringToArray = $this->getMockBuilder('Kata\StringToArray\StringToArray')->getMock();
 
@@ -23,8 +28,21 @@ class MultiLineStringToArrayTest extends \PHPUnit_Framework_TestCase
 			->method('convert');*/
 
 		$multiLineStringToArray = new MultiLineStringToArray(new StringToArray());
-		$result                 = $multiLineStringToArray->convert("211,22,35\n10,20,33");
+		$result                 = $multiLineStringToArray->convert($values);
 
-		$this->assertEquals(array(array('211,22,35', '10,20,33'), array('211','22','35','10','20','33')), $result);
+		$this->assertEquals($expectedArray, $result);
+	}
+
+	/**
+	 * Input values provider.
+	 *
+	 * @return array
+	 */
+	public function inputValuesProvider()
+	{
+		return array(
+			["211,22,35\n10,20,33", array(array('211,22,35', '10,20,33'), array('211','22','35','10','20','33'))],
+			["luxembourg,kennedy,44\nbudapest,expo ter,5-7\ngyors,fo utca,9", array(array('luxembourg,kennedy,44', 'budapest,expo ter,5-7', 'gyors,fo utca,9'), array('luxembourg','kennedy','44','budapest','expo ter','5-7', 'gyors', 'fo utca', '9'))]
+		);
 	}
 }

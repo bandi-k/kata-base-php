ba7bc4d0c18d879bccf0b642e34f64ee97333d2c
bandi-k <andras.kollar@gmail.com>
2016-01-12 21:34:04 +0100

    Add data provider.

--- test/ArrayChop/ArrayChopTest.php
+++ test/ArrayChop/ArrayChopTest.php
@@ -10,10 +10,47 @@ use Kata\ArrayChop\ArrayChop;
 
 class ArrayChopTest extends \PHPUnit_Framework_TestCase
 {
-	public function testArrayChop()
+	/**
+	 * @param int   $result
+	 * @param int   $needle
+	 * @param array $haystack
+	 *
+	 * @dataProvider chopperProvider
+	 */
+	public function testArrayChop($result, $needle, array $haystack)
 	{
 		$arrayChop = new ArrayChop();
 
-		$this->assertEquals(0, $arrayChop->start(0, [0]));
+		$this->assertEquals($result, $arrayChop->start($needle, $haystack));
+	}
+
+	/**
+	 * Chopper data provider.
+	 *
+	 * @return array
+	 */
+	public function chopperProvider()
+	{
+		return array(
+			[-1, 3, []],
+			[-1, 3, [1]],
+			[0,  1, [1]],
+			[0,  1, [1, 3, 5]],
+			[1,  3, [1, 3, 5]],
+			[2,  5, [1, 3, 5]],
+			[-1, 0, [1, 3, 5]],
+			[-1, 2, [1, 3, 5]],
+			[-1, 4, [1, 3, 5]],
+			[-1, 6, [1, 3, 5]],
+			[0,  1, [1, 3, 5, 7]],
+			[1,  3, [1, 3, 5, 7]],
+			[2,  5, [1, 3, 5, 7]],
+			[3,  7, [1, 3, 5, 7]],
+			[-1, 0, [1, 3, 5, 7]],
+			[-1, 2, [1, 3, 5, 7]],
+			[-1, 4, [1, 3, 5, 7]],
+			[-1, 6, [1, 3, 5, 7]],
+			[-1, 8, [1, 3, 5, 7]],
+		);
 	}
 }

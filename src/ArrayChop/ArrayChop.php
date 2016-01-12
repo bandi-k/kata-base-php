50ac2bcb665ec68b879e5b3973d8e4ed96f57cd8
bandi-k <andras.kollar@gmail.com>
2016-01-12 21:31:09 +0100

    Modify start method of ArrayChop class.

--- src/ArrayChop/ArrayChop.php
+++ src/ArrayChop/ArrayChop.php
@@ -8,8 +8,24 @@ namespace Kata\ArrayChop;
 
 class ArrayChop
 {
+	/**
+	 * Returns the index of needle in the haystack or -1 if does not exist.
+	 *
+	 * @param int   $needle     The needle.
+	 * @param array $haystack   the haystack.
+	 *
+	 * @return int   The index.
+	 */
 	public function start($needle, array $haystack)
 	{
-		return 0;
+		foreach($haystack as $key => $item)
+		{
+			if ($needle === $item)
+			{
+				return $key;
+			}
+		}
+
+		return -1;
 	}
 }
